840d5bc17ced7029b2aca797d42cd8c1c767de88
jpbharti <jpbharti@digite.com>
2022-07-07 15:20:50 +0530

    Adding deployment file

---
+++ startserver.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd /opt/infinispan/infinispanserver
+bash bin/server.sh
+#echo '===========start server==========='
+#echo $PORTPLUS
+#bash bin/server.sh -o ${PORTPLUS} -s server
+#echo '===========started==========='
